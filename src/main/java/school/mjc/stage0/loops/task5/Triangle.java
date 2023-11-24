3217485534517263a0c6ebce16b22b4689694528
zayatsdan <36342659+zayatsdan@users.noreply.github.com>
2023-11-24 17:32:43 +0100

    Update Triangle.java

--- src/main/java/school/mjc/stage0/loops/task5/Triangle.java
+++ src/main/java/school/mjc/stage0/loops/task5/Triangle.java
@@ -2,6 +2,28 @@ package school.mjc.stage0.loops.task5;
 
 public class Triangle {
     public void printTriangle(int cathetusLength) {
+         int counterClock=cathetusLength-2;
+        for( int counterY=0; counterY<cathetusLength;counterY++){
+            if(counterY==0){
+                for(int counterX=0;counterX<cathetusLength;counterX++){
+                    System.out.print(8);
+                }
+                System.out.println();
+                continue;
+            }
+            System.out.print(8);
+            for(int empty=counterClock-1;empty>0;empty--){
+                System.out.print(" ");
+            }
+            if(counterY+1!=cathetusLength) {
+                System.out.print(8);
+            }
+            for (int empty=cathetusLength-counterClock-1;empty>0;empty--){
+                System.out.print(" ");
+            }
+            counterClock--;
+            System.out.println();
+        }
 
     }
 }
